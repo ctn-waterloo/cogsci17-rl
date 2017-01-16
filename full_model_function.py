263729581e2f66dd814a9b10ae30df3e1bf5e44d
Brent Komer <brent.komer@gmail.com>
2017-01-16 14:12:18 -0500

    more options for running nengo model

--- full_model_function.py
+++ full_model_function.py
@@ -20,6 +20,9 @@ states = ['S0', 'S1', 'S2']
 
 actions = ['L', 'R']
 
+input_keys = ['S0*L', 'S0*R', 'S1*L', 'S1*R', 'S2*L', 'S2*R']
+output_keys = ['0.7*S1 + 0.3*S2', '0.3*S1 + 0.7*S2', 'S0', 'S0', 'S0', 'S0']
+
 n_sa_neurons = DIM*2*15 # number of neurons in the state+action population
 n_prod_neurons = DIM*15 # number of neurons in the product network
 
@@ -55,79 +58,134 @@ class AreaIntercepts(nengo.dists.Distribution):
             s[i] = self.transform(s[i])
         return s
 
-def get_model(q_scaling=1, direct=False):
+def get_model(q_scaling=1, direct=False, p_learning=True):
 
 
     model = nengo.Network('RL P-learning', seed=13)
+    #if direct:
+    #    model.config[nengo.Ensemble].neuron_type = nengo.Direct()
     if direct:
-        model.config[nengo.Ensemble].neuron_type = nengo.Direct()
+        neuron_type = nengo.Direct()
+    else:
+        neuron_type = nengo.LIF()
 
-    with model:
+    if p_learning:
+        with model:
 
-        # Model of the external environment
-        # Input: action semantic pointer
-        # Output: current state semantic pointer
-        #model.env = nengo.Node(Environment(vocab=vocab, time_interval=time_interval), size_in=DIM, size_out=DIM)
-        agent = Agent(vocab=vocab, time_interval=time_interval,
-                      q_scaling=q_scaling)
-        model.env = nengo.Node(agent,
-                               size_in=1, size_out=DIM*3)
+            # Model of the external environment
+            # Input: action semantic pointer
+            # Output: current state semantic pointer
+            #model.env = nengo.Node(Environment(vocab=vocab, time_interval=time_interval), size_in=DIM, size_out=DIM)
+            agent = Agent(vocab=vocab, time_interval=time_interval,
+                          q_scaling=q_scaling)
+            model.env = nengo.Node(agent,
+                                   size_in=1, size_out=DIM*3)
 
-        model.state = spa.State(DIM, vocab=vocab)
-        model.action = spa.State(DIM, vocab=vocab)
-        model.probability = spa.State(DIM, vocab=vocab)
+            model.state = spa.State(DIM, vocab=vocab)
+            model.action = spa.State(DIM, vocab=vocab)
+            model.probability = spa.State(DIM, vocab=vocab)
 
-        # State and selected action in one ensemble
-        model.state_and_action = nengo.Ensemble(n_neurons=n_sa_neurons, dimensions=DIM*2, intercepts=AreaIntercepts(dimensions=DIM*2))
+            # State and selected action in one ensemble
+            model.state_and_action = nengo.Ensemble(n_neurons=n_sa_neurons, dimensions=DIM*2, intercepts=AreaIntercepts(dimensions=DIM*2))
 
-        #model.cconv = nengo.networks.CircularConvolution(300, DIM)
+            #model.cconv = nengo.networks.CircularConvolution(300, DIM)
 
-        nengo.Connection(model.state.output, model.state_and_action[:DIM])
-        nengo.Connection(model.env[:DIM], model.state_and_action[DIM:])
-        conn = nengo.Connection(model.state_and_action, model.probability.input,
-                                function=lambda x: [0]*DIM,
-                                learning_rule_type=nengo.PES(pre_synapse=z**(-int(time_interval*1000))),
-                               )
+            nengo.Connection(model.state.output, model.state_and_action[:DIM])
+            nengo.Connection(model.env[:DIM], model.state_and_action[DIM:])
+            conn = nengo.Connection(model.state_and_action, model.probability.input,
+                                    function=lambda x: [0]*DIM,
+                                    learning_rule_type=nengo.PES(pre_synapse=z**(-int(time_interval*1000))),
+                                   )
 
 
-        # Semantic pointer for the Q values of each state
-        # In the form of q0*S0 + q1*S1 + q2*S2
-        model.q = spa.State(DIM, vocab=vocab)
-        
-        # Scalar reward value from the dot product of P and Q
-        model.reward = nengo.Ensemble(100, 1)
+            # Semantic pointer for the Q values of each state
+            # In the form of q0*S0 + q1*S1 + q2*S2
+            model.q = spa.State(DIM, vocab=vocab)
+            
+            # Scalar reward value from the dot product of P and Q
+            model.reward = nengo.Ensemble(100, 1, neuron_type=neuron_type)
 
-        #TODO: figure out what the result of P.Q is used for
-        model.prod = nengo.networks.Product(n_neurons=n_prod_neurons, dimensions=DIM)
+            #TODO: figure out what the result of P.Q is used for
+            model.prod = nengo.networks.Product(n_neurons=n_prod_neurons, dimensions=DIM)
 
-        nengo.Connection(model.probability.output, model.prod.A)
-        #nengo.Connection(model.q.output, model.prod.B)
-        nengo.Connection(model.env[DIM*2:], model.prod.B)
+            nengo.Connection(model.probability.output, model.prod.A)
+            #nengo.Connection(model.q.output, model.prod.B)
+            nengo.Connection(model.env[DIM*2:], model.prod.B)
 
-        nengo.Connection(model.prod.output, model.reward,
-                         transform=np.ones((1,DIM)))
+            nengo.Connection(model.prod.output, model.reward,
+                             transform=np.ones((1,DIM)))
 
-        #TODO: doublecheck that this is the correct way to connect things
-        nengo.Connection(model.env[DIM:DIM*2], model.state.input)
+            #TODO: doublecheck that this is the correct way to connect things
+            nengo.Connection(model.env[DIM:DIM*2], model.state.input)
 
-        #TODO: need to set up error signal and handle timing
-        model.error = spa.State(DIM, vocab=vocab)
-        nengo.Connection(model.error.output, conn.learning_rule)
+            #TODO: need to set up error signal and handle timing
+            model.error = spa.State(DIM, vocab=vocab)
+            nengo.Connection(model.error.output, conn.learning_rule)
 
-        #TODO: figure out which way the sign goes, one should be negative, and the other positive
-        #TODO: figure out how to delay by one "time-step" correctly
-        nengo.Connection(model.state.output, model.error.input, transform=-1)
-        nengo.Connection(model.probability.output, model.error.input, transform=1,
-                         synapse=z**(-int(time_interval*1000)))
-                         #synapse=nengolib.synapses.PureDelay(500)) #500ms delay
+            #TODO: figure out which way the sign goes, one should be negative, and the other positive
+            #TODO: figure out how to delay by one "time-step" correctly
+            nengo.Connection(model.state.output, model.error.input, transform=-1)
+            nengo.Connection(model.probability.output, model.error.input, transform=1,
+                             synapse=z**(-int(time_interval*1000)))
+                             #synapse=nengolib.synapses.PureDelay(500)) #500ms delay
 
-        # Testing the delay synapse to make sure it works as expected
-        model.state_delay_test = spa.State(DIM, vocab=vocab)
-        nengo.Connection(model.state.output, model.state_delay_test.input,
-                         synapse=z**(-int(time_interval*1000)))
+            # Testing the delay synapse to make sure it works as expected
+            model.state_delay_test = spa.State(DIM, vocab=vocab)
+            nengo.Connection(model.state.output, model.state_delay_test.input,
+                             synapse=z**(-int(time_interval*1000)))
 
-        nengo.Connection(model.reward, model.env)
-        nengo.Connection(model.env[:DIM], model.action.input) # Purely for plotting
-        nengo.Connection(model.env[DIM*2:], model.q.input) # Purely for plotting
+            nengo.Connection(model.reward, model.env)
+            nengo.Connection(model.env[:DIM], model.action.input) # Purely for plotting
+            nengo.Connection(model.env[DIM*2:], model.q.input) # Purely for plotting
 
-    return model, agent
+        return model, agent
+    else:
+        with model:
+            agent = Agent(vocab=vocab, time_interval=time_interval,
+                          q_scaling=q_scaling)
+            model.env = nengo.Node(agent,
+                                   size_in=1, size_out=DIM*3)
+
+            model.assoc_mem = spa.AssociativeMemory(input_vocab=vocab,
+                                                    output_vocab=vocab,
+                                                    input_keys=input_keys,
+                                                    output_keys=output_keys,
+                                                    wta_output=True,
+                                                    threshold_output=True
+                                                   )
+
+            #model.state = spa.State(DIM, vocab=vocab)
+            #model.action = spa.State(DIM, vocab=vocab)
+            model.probability = spa.State(DIM, vocab=vocab)
+
+            # State and selected action convolved together
+            #model.state_and_action = spa.State(DIM, vocab=vocab)
+
+            model.cconv = nengo.networks.CircularConvolution(300, DIM)
+
+            #nengo.Connection(model.state.output, model.cconv.A)
+            #nengo.Connection(model.action.output, model.cconv.B)
+            nengo.Connection(model.env[DIM:DIM*2], model.cconv.A)
+            nengo.Connection(model.env[:DIM], model.cconv.B)
+            nengo.Connection(model.cconv.output, model.assoc_mem.input)
+            nengo.Connection(model.assoc_mem.output, model.probability.input)
+
+            # Semantic pointer for the Q values of each state
+            # In the form of q0*S0 + q1*S1 + q2*S2
+            model.q = spa.State(DIM, vocab=vocab)
+            
+            # Scalar reward value from the dot product of P and Q
+            model.reward = nengo.Ensemble(100, 1, neuron_type=neuron_type)
+
+            #TODO: figure out what the result of P.Q is used for
+            model.prod = nengo.networks.Product(n_neurons=15*DIM, dimensions=DIM)
+
+            nengo.Connection(model.probability.output, model.prod.A)
+            #nengo.Connection(model.q.output, model.prod.B)
+            nengo.Connection(model.env[DIM*2:], model.prod.B)
+
+            nengo.Connection(model.prod.output, model.reward,
+                             transform=np.ones((1,DIM)))
+            
+            nengo.Connection(model.reward, model.env)
+        return model, agent
--- gather_nengo_data.py
+++ gather_nengo_data.py
@@ -11,20 +11,35 @@ num_runs = 3#100
 num_steps = 40#40000
 tf_name = 'temp_file.txt'
 
+# Running some ensembles in Direct mode
+direct = True
+
+# If the transition probabilities should be learned
+p_learning = False
+
 # Read option parameters from the command line
+if len(sys.argv) == 5:
+    num_runs = int(sys.argv[1])
+    num_steps = int(sys.argv[2])
+    direct = sys.argv[3] == 'True'
+    p_learning = sys.argv[4] == 'True'
+if len(sys.argv) == 4:
+    num_runs = int(sys.argv[1])
+    num_steps = int(sys.argv[2])
+    direct = sys.argv[3] == 'True'
 if len(sys.argv) == 3:
     num_runs = int(sys.argv[1])
     num_steps = int(sys.argv[2])
 if len(sys.argv) == 2:
     num_runs = int(sys.argv[1])
 
-outfile_name = 'data/out_nengo_r{0}_s{1}.txt'.format(num_runs, num_steps)
+outfile_name = 'data/out_nengo_r{0}_s{1}_d{2}_p{3}.txt'.format(num_runs, num_steps, direct, p_learning)
 with open(outfile_name, 'w+') as outfile:
 
     for i in range(num_runs):
         print('{0}/{1} Runs'.format(i,num_runs))
 
-        model, agent = get_model()
+        model, agent = get_model(direct=direct, p_learning=p_learning)
 
         sim = nengo.Simulator(model)
         sim.run(num_steps*.1)
